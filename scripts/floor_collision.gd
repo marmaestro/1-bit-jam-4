5354cf43c3262efe2a934ad362215aecdea62a2f
Mar Maestro <marmaestromomparler@gmail.com>
2024-10-06 19:48:41 +0200

    New meshes
    Not working:
    - mesh scalling (make bigger, 2)
    - wall collider
    - switching uvs
    - playthrough?

--- scripts/floor_collision.gd
+++ scripts/floor_collision.gd
@@ -3,4 +3,4 @@ extends Node3D
 @onready var rigidbody = $rigidbody
 
 func _on_rigidbody_body_shape_entered(_body_rid, _body, _body_shape_index, _local_shape_index):
-	rigidbody.contact_monitor = false
+	rigidbody.call_deferred("set_contact_monitor", false)
--- scripts/floor_handler.gd
+++ scripts/floor_handler.gd
@@ -4,9 +4,9 @@ var survived_floors = -1
 var rng = RandomNumberGenerator.new()
 @onready var timer = $"../Timer"
 
-@onready var new_floor_scene = preload("res://prefabs/new_floor.tscn")
-@onready var floor_meshes = [preload("res://models/floor_1.obj"), preload("res://models/floor_2.obj"), preload("res://models/floor_3.obj"), preload("res://models/floor_4.obj")]
-@onready var floor_uvs = [preload("res://materials/floor_1_uv.png"), preload("res://materials/floor_2_uv.png"), preload("res://materials/floor_3_uv.png"), preload("res://materials/floor_4_uv.png")]
+@onready var new_floor_scene = preload("res://scenes/new_floor.tscn")
+@onready var floor_meshes = [preload("res://models/chip_1.obj"), preload("res://models/chip_2.obj"), preload("res://models/chip_3.obj"), preload("res://models/chip_4.obj"), preload("res://models/chip_5.obj")]
+@onready var floor_uvs = [preload("res://materials/chip_1_uv.png"), preload("res://materials/chip_2_uv.png"), preload("res://materials/chip_3_uv.png"), preload("res://materials/chip_4_uv.png"), preload("res://materials/chip_5_uv.png")]
 
 var new_floor
 
@@ -19,12 +19,12 @@ func _on_timeout():
 	
 func create_floor():
 	new_floor = new_floor_scene.instantiate()
-	var floor_n = randi_range(0, 3)
+	var floor_n = randi_range(0, 4)
 	new_floor.name = str(Time.get_ticks_msec())
 	
 	new_floor.get_child(0).rotate_y(rng.randf_range(-180.0, 180.0))
 	new_floor.get_child(0).get_child(1).mesh = floor_meshes[floor_n]
-	new_floor.get_child(0).get_child(1).mesh.surface_set_material(0, floor_uvs[floor_n], )
+	new_floor.get_child(0).get_child(1).mesh.surface_set_material(0, floor_uvs[floor_n])
 	
 	new_floor.get_child(0).get_child(0).make_convex_from_siblings()	
 	add_child(new_floor)
